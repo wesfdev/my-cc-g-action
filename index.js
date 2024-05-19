5339738431a3de0fb3ec2f3c5e41406ef19ecb9d
Wes <wesly_fernandez1@outlook.com>
2024-05-19 09:00:30 -0600

    chore: update call

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const github = require('@actions/github');
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const octokit = github.getOctokit(GITHUB_TOKEN);
+    //const octokit = github.getOctokit(GITHUB_TOKEN);
     const { context = {} } = github;
     const { pull_request } = context.payload;
 
-    await octokit.rest.issues.createComment({
+    await github.rest.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
         body: 'Te current code coverage is: 80% | [Coverage has not changed | Coverage has decreased by 1% | Coverage has increased by 1%]'

031b9277233bfd6ad2453e5fcf562cee7db1983b
Wes <wesly_fernandez1@outlook.com>
2024-05-19 13:14:15 -0600

    chore: add axios

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const PUSHED_GITHUB_SHA = core.getInput('PUSHED_GITHUB_SHA');
     const GITHUB_BRANCH = core.getInput('GITHUB_BRANCH');
+    const COVERALLS_REPO_TOKEN = core.getInput('COVERALLS_REPO_TOKEN');
+    const COVERALLS_URL = core.getInput('COVERALLS_URL');
+
     const octokit = github.getOctokit(GITHUB_TOKEN);
     const { context = {} } = github;
     const { pull_request } = context.payload;
+
     console.log('PUSHED_GITHUB_SHA', PUSHED_GITHUB_SHA);
     console.log('GITHUB_BRANCH', GITHUB_BRANCH);
+
+    const requestBody = {
+        "key": COVERALLS_REPO_TOKEN,
+        "branch": GITHUB_BRANCH,
+        "head": {
+            "id": PUSHED_GITHUB_SHA
+        }
+    }
+    const {data} = await axios.post(`${COVERALLS_URL}/api/v1/coverage-diff`, requestBody, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    })
+
+    console.log('data', data);
 
     await octokit.rest.issues.createComment({
         ...context.repo,
